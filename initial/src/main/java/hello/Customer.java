9856a6ee7cafc91efeab223f929516597046f49e
shlomitArgov <shlomit.argov@gmail.com>
2017-07-08 13:56:17 +0300

    Add getters + setters for Customer POJO entity

--- initial/src/main/java/hello/Customer.java
+++ initial/src/main/java/hello/Customer.java
@@ -28,4 +28,24 @@ public class Customer {
                 id, firstName, lastName);
     }
 
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
 }
